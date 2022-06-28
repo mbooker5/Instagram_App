e35ae107672b0932e401e984f3923a41704b664d
mbooker5 <mbooker734@gmail.com>
2022-06-28 15:22:00 -0700

    Added Feature that allows user to upload picture from camera or photo library (Build and run)

--- FBU_Instagram/View Controllers/PhotoMapViewController.m
+++ FBU_Instagram/View Controllers/PhotoMapViewController.m
@@ -32,14 +32,11 @@
 */
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
-    
     // Get the image captured by the UIImagePickerController
     UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
-    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
-
+//    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
     // Do something with the images (based on your use case)
     _imageToPost.image = originalImage;
-    
     // Dismiss UIImagePickerController to go back to your original view controller
     [self dismissViewControllerAnimated:YES completion:nil];
 }
@@ -50,22 +47,43 @@
 - (IBAction)didTapShare:(id)sender {
 }
 - (IBAction)didTapImage:(id)sender {
+    
+    
     UIImagePickerController *imagePickerVC = [UIImagePickerController new];
     imagePickerVC.delegate = self;
     imagePickerVC.allowsEditing = YES;
     
 
     // The Xcode simulator does not support taking pictures, so let's first check that the camera is indeed supported on the device before trying to present it.
-    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
-        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
-    }
-    else {
-        NSLog(@"Camera 🚫 available so we will use photo library instead");
+    
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Upload Image"
+                                   message:@""
+                                   preferredStyle:UIAlertControllerStyleAlert];
+     
+    UIAlertAction* useCamera = [UIAlertAction actionWithTitle:@"Camera" style:UIAlertActionStyleDefault
+        handler:^(UIAlertAction * action){
+        if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+            imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+            }
+        else {
+            imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        }
+        [self presentViewController:imagePickerVC animated:YES completion:nil];
+    }];
+    
+    UIAlertAction* usePhotoLibrary = [UIAlertAction actionWithTitle:@"Photo Library" style:UIAlertActionStyleDefault
+        handler:^(UIAlertAction * action){
         imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-    }
-
-    [self presentViewController:imagePickerVC animated:YES completion:nil];
+        [self presentViewController:imagePickerVC animated:YES completion:nil];
+    }];
+    
+    
+    [alert addAction:useCamera];
+    [alert addAction:usePhotoLibrary];
+    [self presentViewController:alert animated:YES completion:nil];
+    
 }
+
 
 
 @end
